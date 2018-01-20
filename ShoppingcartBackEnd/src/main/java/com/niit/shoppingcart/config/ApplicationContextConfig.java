8e0eef5ab912c3303789a2fad273f8b7fcc8dea5
naresh <naresh@192.168.0.4>
2018-01-21 02:00:14 +0530

    backend

--- ShoppingcartBackEnd/src/main/java/com/niit/shoppingcart/config/ApplicationContextConfig.java
+++ ShoppingcartBackEnd/src/main/java/com/niit/shoppingcart/config/ApplicationContextConfig.java
@@ -32,7 +32,7 @@ public class ApplicationContextConfig {
 
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-		dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+		dataSource.setUrl("jdbc:h2:tcp://localhost/~/os");
 		dataSource.setDriverClassName("org.h2.Driver");
 		dataSource.setUsername("sa");
 		dataSource.setPassword("");
